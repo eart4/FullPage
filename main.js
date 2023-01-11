3f1c0986239b9f170c93ae1f15ef3887d2fe7132
Philipp <91840260+eart4@users.noreply.github.com>
2023-01-11 15:06:50 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+const burger = document.querySelector('.burger')
+const container = document.querySelector('.container')
+burger.addEventListener('click',() =>{
+        container.classList.toggle('active')
+})
